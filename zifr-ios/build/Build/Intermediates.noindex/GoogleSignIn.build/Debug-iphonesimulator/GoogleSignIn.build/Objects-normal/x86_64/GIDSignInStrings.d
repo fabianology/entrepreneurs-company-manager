35103210ea6a671fc8f5052e84578dcd58a53924
fabianology <29523454+fabianology@users.noreply.github.com>
2026-08-24 15:00:32 -0700

    chore(ios): stop tracking generated Xcode files

--- zifr-ios/build/Build/Intermediates.noindex/GoogleSignIn.build/Debug-iphonesimulator/GoogleSignIn.build/Objects-normal/x86_64/GIDSignInStrings.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/yager/Downloads/cifr---entrepreneur's-company-manager/zifr-ios/build/SourcePackages/checkouts/GoogleSignIn-iOS/GoogleSignIn/Sources/GIDSignInStrings.m \
-  /Users/yager/Downloads/cifr---entrepreneur's-company-manager/zifr-ios/build/Build/Intermediates.noindex/GoogleSignIn.build/Debug-iphonesimulator/GoogleSignIn.build/DerivedSources/resource_bundle_accessor.h \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/System/Library/Frameworks/Foundation.framework/Modules/module.modulemap \
-  /Users/yager/Downloads/cifr---entrepreneur's-company-manager/zifr-ios/build/SourcePackages/checkouts/GoogleSignIn-iOS/GoogleSignIn/Sources/GIDSignInStrings.h \
-  /Users/yager/Downloads/cifr---entrepreneur's-company-manager/zifr-ios/build/SourcePackages/checkouts/GoogleSignIn-iOS/GoogleSignIn/Sources/NSBundle+GID3PAdditions.h
